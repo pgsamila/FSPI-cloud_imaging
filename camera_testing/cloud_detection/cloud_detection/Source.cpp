e8357f69b71114191a923066e15c00559c8e36ce
Amila Sampath <pgasampath@gmail.com>
2017-04-18 04:53:24 +0530

    code testing at train

--- camera_testing/cloud_detection/cloud_detection/Source.cpp
+++ camera_testing/cloud_detection/cloud_detection/Source.cpp
@@ -23,7 +23,7 @@ int main (void)
 	VideoCapture vcap;
 
 
-	vcap.open("http://192.168.137.64:8080/video"); // ip address of the video stream
+	vcap.open("http://192.168.42.129:8080/video"); // ip address of the video stream
 
 	if (!vcap.isOpened())// if video streaming does not catched
 	{
@@ -68,85 +68,58 @@ int main (void)
 	while(1)
 	{
 		bool b_video_play = vcap.read(first_frame);
-		int x = 0;
-		if (!took_first_frame) // take the first refference frame
-		{
-			while (x < 1)
-			{
-				b_video_play = vcap.read(first_frame);
-				if (!b_video_play)
-				{
-					cout << "Cannot Read a frame" << endl;
-					break;
-				}
-				x++;
-			}
-		}
-	
-		x = 0;
-
-		b_video_play = vcap.read(frame); // read a new frame from video
-
-		while (x < 1) {
-			b_video_play = vcap.read(frame);
-			if (!b_video_play)
-			{
-				cout << "Cannot Read a frame" << endl;
-				break;
-			}
-			x++;
-		}
 		
+		b_video_play = vcap.read(frame);
 		if (!b_video_play)
 		{
 			cout << "Cannot Read a frame" << endl;
 			break;
 		}
-
+		
 		//flip(frame, frame, 1);
 		imshow("original_video", frame);
 
-		Canny(frame, contour, 500, 1000, 5, true);
+		//Canny(frame, contour, 500, 1000, 5, true);
 		//imshow("negative_video", contour);
 
 		/// Up to now, the streaming video frames are recived.
 		
-		Mat img_frame1 = frame;
-		Mat img_frame2 = first_frame;
+		//Mat img_frame1 = frame;
+		//Mat img_frame2 = first_frame;
 		Mat img_original = frame;
-		Mat img_defference;
-		Mat img_treshold;
-		cvtColor(img_frame1, img_frame1, CV_BGR2GRAY);
-		cvtColor(img_frame2, img_frame2, CV_BGR2GRAY);
+		//Mat img_defference;
+		//Mat img_treshold;
+		//cvtColor(img_frame1, img_frame1, CV_BGR2GRAY);
+		//cvtColor(img_frame2, img_frame2, CV_BGR2GRAY);
 		//imshow("GrayScale",img_frame1);
-		GaussianBlur(img_frame1, img_frame1, Size(5, 5), 0);
-		GaussianBlur(img_frame2, img_frame2, Size(5, 5), 0);
+		//GaussianBlur(img_frame1, img_frame1, Size(5, 5), 0);
+		//GaussianBlur(img_frame2, img_frame2, Size(5, 5), 0);
 		//imshow("GaussianBlur", img_frame1);
-		absdiff(img_frame1, img_frame2, img_defference);
+		//absdiff(img_frame1, img_frame2, img_defference);
 		//imshow("Defference", img_defference); 
 
-		threshold(img_defference, img_treshold, 50, 255.0, CV_THRESH_BINARY);
+		//threshold(img_defference, img_treshold, 50, 255.0, CV_THRESH_BINARY);
 
-		threshold(img_defference, img_treshold, 50, 255.0, CV_THRESH_BINARY);
-		threshold(img_frame2, img_treshold, 150, 255.0, CV_THRESH_BINARY);
+		//threshold(img_defference, img_treshold, 50, 255.0, CV_THRESH_BINARY);
+		//threshold(img_frame2, img_treshold, 150, 255.0, CV_THRESH_BINARY);
 		//imshow("Threshold", img_treshold);
 		
-		Mat structuringElement3x3 = getStructuringElement(MORPH_RECT, Size(3, 3));
-		Mat structuringElement5x5 = getStructuringElement(MORPH_RECT, Size(5, 5));
-		Mat structuringElement7x7 = getStructuringElement(MORPH_RECT, Size(7, 7));
-		Mat structuringElement9x9 = getStructuringElement(MORPH_RECT, Size(9, 9));
+		//Mat structuringElement3x3 = getStructuringElement(MORPH_RECT, Size(3, 3));
+		//Mat structuringElement5x5 = getStructuringElement(MORPH_RECT, Size(5, 5));
+		//Mat structuringElement7x7 = getStructuringElement(MORPH_RECT, Size(7, 7));
+		//Mat structuringElement9x9 = getStructuringElement(MORPH_RECT, Size(9, 9));
 
-		dilate(img_treshold, img_treshold, structuringElement5x5);
-		dilate(img_treshold, img_treshold, structuringElement5x5);
-		erode(img_treshold, img_treshold, structuringElement5x5);
+		//dilate(img_treshold, img_treshold, structuringElement5x5);
+		//dilate(img_treshold, img_treshold, structuringElement5x5);
+		//erode(img_treshold, img_treshold, structuringElement5x5);
 
-		Mat img_treshold_copy = img_treshold.clone();
+		//Mat img_treshold_copy = img_treshold.clone();
 
-		vector<vector<Point> > contours;
+		//vector<vector<Point> > contours;
 
 		//findContours(img_treshold_copy, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
-		Mat img_contours(img_treshold_copy.size(), CV_8UC3, SCALAR_BLACK);
+		//Mat img_contours(img_treshold_copy.size(), CV_8UC3, SCALAR_BLACK);
 
 		//drawContours(img_contours, contours, -1, SCALAR_WHITE, -1);
 
@@ -160,7 +133,7 @@ int main (void)
 		Mat imgHSV;
 		
 		cvtColor(img_original, imgHSV, COLOR_BGR2HSV);
-		imshow("HSV imga", imgHSV);
+		//imshow("HSV imga", imgHSV);
 		Mat img_TresholdHSV;
 
 		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), img_TresholdHSV);
@@ -173,10 +146,20 @@ int main (void)
 		//imshow("Threshold img", img_TresholdHSV);
 
 		
+		//for (int row = 0; row < 160; row++) {
+			//for (int cols = 0; cols < 240; cols++) {
+				//Vec3b oldOne = imgHSV.at<Vec3b>(row, cols);
+				//for (int z = 0; z < 255; z++) {
+					//if (imgHSV.at<Vec3b>(row, cols) == Vec3b(0, 0, 200)) {
+						//Vec3b newOne = Vec3b(0, 0, 0);
+						//imgHSV.at<Vec3b>(row, cols) = Vec3b(0, 0, 0);// = '0';
+					//}
+				//}
+			//}
+		//}
+		cvtColor(imgHSV, imgHSV, CV_BGR2GRAY);
 
-
-
-
+		imshow("HSV imga", imgHSV);
 		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
 		{
 			cout << "esc key is pressed by user" << endl;
